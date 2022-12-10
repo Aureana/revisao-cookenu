94f3c9d3b09b20457fcae0dd7be3d60f1a1b5399
Aureana <aureana2018@outlook.com>
2022-12-10 19:44:01 -0300

    Cookenu concluido

---
+++ src/routes/coordinator.js
@@ -0,0 +1,15 @@
+export const goToHomePage = (navigate) => {
+    navigate("/")
+}
+
+export const goToSignupPage = (navigate) => {
+    navigate("/signup")
+}
+
+export const goToLoginPage = (navigate) => {
+    navigate("/login")
+}
+
+export const goToDetailsPage = (navigate, recipeId) => {
+    navigate(`/recipe/${recipeId}`)
+}
